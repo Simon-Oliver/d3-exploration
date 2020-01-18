5c5bf3e604e91d375acab11d700542743df482b0
Simon-Oliver <stauffer.simon@outlook.com>
2020-01-18 10:17:51 +0100

    basic color coding of temp based on dummy data

--- d3_test/index.js
+++ d3_test/index.js
@@ -2,4 +2,24 @@ d3.selectAll('p').style('color', 'blue');
 
 fetch('./data.json')
   .then(res => res.json())
-  .then(data => console.log(data));
+  .then(data => {
+    console.log(data);
+    var p = d3
+      .select('body')
+      .selectAll('p')
+      .data(data)
+      .enter()
+      .append('p')
+      .style('color', d => {
+        if (d.Mean < 0) {
+          return 'grey';
+        } else if (d.Mean >= 0.0 && d.Mean <= 0.5) {
+          return 'green';
+        } else {
+          return 'red';
+        }
+      })
+      .text(function(d, i) {
+        return d.Mean;
+      });
+  });
