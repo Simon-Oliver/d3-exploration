5429912f4f6239da915ce2489938ab53d918a0e3
Simon-Oliver <stauffer.simon@outlook.com>
2020-01-21 22:15:49 +0100

    added geolocation

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
-import Graph from './Graph';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h2>Temperature</h2>
-        <Graph></Graph>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getPosition);
+    }
+    function getPosition(position) {
+      console.log(position.coords.latitude, position.coords.longitude);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <h2>Temperature</h2>
+          <button onClick={console.log(this.usePosition)}></button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
