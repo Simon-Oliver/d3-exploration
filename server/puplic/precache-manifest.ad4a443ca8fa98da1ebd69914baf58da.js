162db115688e45415452d16a55a0344aa7f0bf25
Simon-Oliver <stauffer.simon@outlook.com>
2020-01-23 21:01:38 +0100

    heroku deploy

---
+++ server/puplic/precache-manifest.ad4a443ca8fa98da1ebd69914baf58da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92eadbbbb67fddcc2761507b903d849d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8ff776cf44fa7c549ed",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c602889a52c79ca934b",
+    "url": "/static/js/2.b2ced33c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b2ced33c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8ff776cf44fa7c549ed",
+    "url": "/static/js/main.1bd6ddfc.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
